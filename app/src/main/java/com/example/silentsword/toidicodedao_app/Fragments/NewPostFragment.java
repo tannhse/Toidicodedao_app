bc051105b2ad39cc4dba36a6cc9af41304c03569
Tân Nguyễn <tannhdev@gmail.com>
2016-12-10 12:37:09 +0700

    Add Classes and XMLs

---
+++ app/src/main/java/com/example/silentsword/toidicodedao_app/Fragments/NewPostFragment.java
@@ -0,0 +1,27 @@
+package com.example.silentsword.toidicodedao_app.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.silentsword.toidicodedao_app.MainActivity;
+import com.example.silentsword.toidicodedao_app.R;
+
+/**
+ * Created by SilentSword on 10/12/2016.
+ */
+
+public class NewPostFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_newpost, container, false);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Bài viết mới");
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/silentsword/toidicodedao_app/MainActivity.java
+++ app/src/main/java/com/example/silentsword/toidicodedao_app/MainActivity.java
@@ -1,13 +1,168 @@
 package com.example.silentsword.toidicodedao_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.silentsword.toidicodedao_app.Fragments.CarrierFragment;
+import com.example.silentsword.toidicodedao_app.Fragments.CodingFragment;
+import com.example.silentsword.toidicodedao_app.Fragments.KnowledgeFragment;
+import com.example.silentsword.toidicodedao_app.Fragments.NewPostFragment;
+import com.example.silentsword.toidicodedao_app.Fragments.StoriesFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        if (navigationView != null) {
+            setupNavigationDrawerContent(navigationView);
+        }
+
+        setupNavigationDrawerContent(navigationView);
+
+        //First fragment
+        setFragment(0);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupNavigationDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.item_navigation_drawer_newpost:
+                                menuItem.setChecked(true);
+                                setFragment(0);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_coding:
+                                menuItem.setChecked(true);
+                                setFragment(1);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_carrier:
+                                menuItem.setChecked(true);
+                                setFragment(2);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_stories:
+                                menuItem.setChecked(true);
+                                setFragment(3);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_knowledge:
+                                menuItem.setChecked(true);
+                                setFragment(4);
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_settings:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, "Launching " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                                startActivity(intent);
+                                return true;
+                            case R.id.item_navigation_drawer_help_and_feedback:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_about_us:
+                                menuItem.setChecked(true);
+                                Toast.makeText(MainActivity.this, menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+
+                        }
+                        return true;
+                    }
+                });
+    }
+
+    public void setFragment(int position) {
+        FragmentManager fragmentManager;
+        FragmentTransaction fragmentTransaction;
+        switch (position) {
+            case 0:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                NewPostFragment newPostFragment = new NewPostFragment();
+                fragmentTransaction.replace(R.id.fragment, newPostFragment);
+                fragmentTransaction.commit();
+                break;
+            case 1:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                CodingFragment codingFragment = new CodingFragment();
+                fragmentTransaction.replace(R.id.fragment, codingFragment);
+                fragmentTransaction.commit();
+                break;
+            case 2:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                CarrierFragment carrierFragment = new CarrierFragment();
+                fragmentTransaction.replace(R.id.fragment, carrierFragment);
+                fragmentTransaction.commit();
+                break;
+            case 3:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                StoriesFragment storiesFragment = new StoriesFragment();
+                fragmentTransaction.replace(R.id.fragment, storiesFragment);
+                fragmentTransaction.commit();
+                break;
+            case 4:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                KnowledgeFragment knowledgeFragment = new KnowledgeFragment();
+                fragmentTransaction.replace(R.id.fragment, knowledgeFragment);
+                fragmentTransaction.commit();
+                break;
+        }
     }
 }
